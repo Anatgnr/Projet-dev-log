a82f6876dcd7ea2bdd3fd070db68d8e5cc3aee62
Anatole GARNIER <anatole.garnier@etu.u-bordeaux.fr>
2023-01-23 15:02:18 +0100

    http-api ok

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,46 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import axios, { AxiosResponse } from 'axios'
-
-let id = 0
+import { print, call, fct, ListImage, IdImage, count } from './http-api'
 
 defineProps<{ msg: "hallp" }>()
-
-const count = ref(0)
-const ListImage = ref([{ "name": "", "id": 0 }])
-
-axios.get('/images')
-  .then(function (response: AxiosResponse) {
-    ListImage.value = (response.data);
-    console.log(ListImage.value);
-  })
-  .catch(error => { console.log(error) })
-
-let IdImage: { id: number }
-
-function fct() {
-  console.log(IdImage)
-}
-
-var imageUrl = "/images/";
-var imageEL;
-
-function print(nbid: number, imageUrl: string) {
-  axios.get(imageUrl + nbid, { responseType: "blob" })
-    .then(function (response: AxiosResponse) {
-      const reader = new window.FileReader();
-      reader.readAsDataURL(response.data);
-      reader.onload = function () {
-        const imageDataUrl = (reader.result as string);
-        imageEL = document.getElementById("img");
-        imageEL.setAttribute("src", imageDataUrl);
-      }
-    })
-    .catch(error => { console.log(error) });
-}
-function call() {
-  print(IdImage.id, imageUrl)
-}
 
 </script>
 
--- frontend/src/components/http-api.ts
+++ frontend/src/components/http-api.ts
@@ -1,45 +1,40 @@
-// import { defineComponent } from 'vue'
-// import { ref } from 'vue'
-// import axios, { AxiosResponse } from 'axios'
+import { ref } from 'vue'
+import axios, { AxiosResponse } from 'axios'
 
-// export default defineComponent({
-// let id = 0
+export let id = 0
 
-// defineProps<{ msg: "hallp" }>()
+export const count = ref(0)
+export const ListImage = ref([{ "name": "", "id": 0 }])
 
-// const count = ref(0)
-// const ListImage = ref([{ "name": "", "id": 0 }])
+axios.get('/images')
+    .then(function (response: AxiosResponse) {
+        ListImage.value = (response.data);
+        console.log(ListImage.value);
+    })
+    .catch(error => { console.log(error) })
 
-// axios.get('/images')
-//     .then(function (response: AxiosResponse) {
-//         ListImage.value = (response.data);
-//         console.log(ListImage.value);
-//     })
-//     .catch(error => { console.log(error) })
+export let IdImage: { id: number }
 
-// let IdImage: { id: number }
+export function fct() {
+    console.log(IdImage)
+}
 
-// function fct() {
-//     console.log(IdImage)
-// }
+export var imageUrl = "/images/";
+export var imageEL;
 
-// var imageUrl = "/images/";
-// var imageEL;
-
-// function print(nbid: number, imageUrl: string) {
-//     axios.get(imageUrl + nbid, { responseType: "blob" })
-//         .then(function (response: AxiosResponse) {
-//             const reader = new window.FileReader();
-//             reader.readAsDataURL(response.data);
-//             reader.onload = function () {
-//                 const imageDataUrl = (reader.result as string);
-//                 imageEL = document.getElementById("img");
-//                 imageEL.setAttribute("src", imageDataUrl);
-//             }
-//         })
-//         .catch(error => { console.log(error) });
-// }
-// function call() {
-//     print(IdImage.id, imageUrl)
-// }
-// })
+export function print(nbid: number, imageUrl: string) {
+    axios.get(imageUrl + nbid, { responseType: "blob" })
+        .then(function (response: AxiosResponse) {
+            const reader = new window.FileReader();
+            reader.readAsDataURL(response.data);
+            reader.onload = function () {
+                const imageDataUrl = (reader.result as string);
+                imageEL = document.getElementById("img");
+                imageEL.setAttribute("src", imageDataUrl);
+            }
+        })
+        .catch(error => { console.log(error) });
+}
+export function call() {
+    print(IdImage.id, imageUrl)
+}
